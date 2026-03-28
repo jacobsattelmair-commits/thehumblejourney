c869159792eb652aafe8161aaa0bef248fda7349
jacobsattelmair-commits <jacobsattelmair@gmail.com>
2026-03-27 21:26:15 -0400

    Update whoop-auth.js

--- netlify/functions/whoop-auth.js
+++ netlify/functions/whoop-auth.js
@@ -2,7 +2,7 @@
 export default async function handler(req) {
   const clientId = process.env.WHOOP_CLIENT_ID;
   const redirectUri = 'https://thehumblejourney.com/.netlify/functions/whoop-callback';
-  const scopes = 'read:recovery read:sleep read:workout read:profile read:body_measurement offline';
+  const scopes = 'read:recovery read:cycles read:sleep read:profile read:body_measurement offline';
   const state = crypto.randomUUID().replace(/-/g, '');
 
   const authUrl = 'https://api.prod.whoop.com/oauth/oauth2/auth' +
